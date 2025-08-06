f27a635ee17bf36cfb6294c4f9b69f82dd62e5a9
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-08-06 09:33:57 +0200

    Fix all merge conflicts in frontend and backend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,23 @@
-<<<<<<< HEAD
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatInterface from './chatbot/ChatInterface'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Shafii's Portfolio</h1>
-        <p>Welcome to my portfolio! Feel free to chat with me.</p>
-      </header>
-      <main>
-        <ChatInterface />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Shafii's Portfolio</h1>
+          <p>Welcome to my portfolio! Feel free to chat with me.</p>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<ChatInterface />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
-export default App 
-=======
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
-import MainLayout from "./layouts/MainLayout";
-import HomePage from "./pages/HomePage";
-import ProjectsPage from "./pages/ProjectsPage";
-import ProjectDetailPage from "./pages/ProjectDetailPage";
-import ContactPage from "./pages/ContactPage";
-import CVPage from "./pages/CVPage";
-
-const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-      <Router>
-        <MainLayout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/projects/:id" element={<ProjectDetailPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/cv" element={<CVPage />} />
-          </Routes>
-        </MainLayout>
-      </Router>
-    </ThemeProvider>
-  );
-};
-
-export default App;
->>>>>>> b21f2a90b765a4a59be96053cc30eab67046896b
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -8,19 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-) 
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { ThemeProvider } from "./context/ThemeContext";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
->>>>>>> b21f2a90b765a4a59be96053cc30eab67046896b
+)
