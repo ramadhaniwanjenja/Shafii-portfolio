38e10756a76254e8648e0c70d6b6bfe314570842
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-08-07 21:31:19 +0200

    Fix all merge conflicts in frontend and backend files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: "class",
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: "1rem",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
